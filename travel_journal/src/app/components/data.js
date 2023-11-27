1c8dfe32c79cb32fd2a6deb3e2a44ad0eb4f15f5
enoskyeza <enoskyeza@gmail.com>
2023-11-27 22:47:25 +0300

    Add location sample data

---
+++ travel_journal/src/app/components/data.js
@@ -0,0 +1,11 @@
+export default[
+    {
+        id: 1,
+        img: mt-fuji,
+        country: Japan,
+        map: "https://www.google.com/maps/place/Mount+Fuji/@35.3606247,138.7186086,15z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634?shorturl=1",
+        place: 'Mount Fuji',
+        period: '12 Jan, 2021 - 24 Jan, 2021',
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    }
+]
