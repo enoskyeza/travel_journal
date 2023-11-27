41987a56454921c4c282b83c4a4a5a4176a94687
enoskyeza <enoskyeza@gmail.com>
2023-11-27 22:27:53 +0300

    Add footer section

---
+++ travel_journal/src/app/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p><small>Coded by Enos Kitooke</small></p>
+        </footer>
+    )
+}
