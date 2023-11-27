e17d97958e62cc2f93bfdc6cf2d56f61944106f2
enoskyeza <enoskyeza@gmail.com>
2023-11-27 23:31:24 +0300

    Add destination styles

--- travel_journal/src/app/components/Destination.js
+++ travel_journal/src/app/components/Destination.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { FaLocationDot } from "react-icons/fa6";
 
 export default function Destination(props) {
-    return (
-        <>
-        <img />
-        <div>
-            <div>
-                <img src={`./${props.img}`} alt="location-icon" />
-                <p>{props.country}</p>
-                <a href={props.map}>View on Google Maps</a>
-            </div>
-            <h2>{props.place}</h2>
-            <h6>{props.period}</h6>
-            <p>{props.description}</p>
+  return (
+    <div className="place-content">
+      <img src={`./${props.img}`} alt="location" />
+      <div className="place-content-right">
+        <div className="place-directions">
+          <FaLocationDot />
+          <p>{props.country}</p>
+          <a href={props.map}>View on Google Maps</a>
         </div>
-        </>
-    )
+        <h2>{props.place}</h2>
+        <h6>{props.period}</h6>
+        <p>{props.description}</p>
+      </div>
+    </div>
+  );
 }
