9434a841827d0a65e4cdd6de73a7048406434b4b
enoskyeza <enoskyeza@gmail.com>
2023-11-28 00:17:09 +0300

    Add places data

--- travel_journal/src/app/components/Destination.js
+++ travel_journal/src/app/components/Destination.js
@@ -3,18 +3,22 @@ import { FaLocationDot } from "react-icons/fa6";
 
 export default function Destination(props) {
   return (
-    <div className="place-content">
-      <img src={`./${props.img}`} alt="location" />
-      <div className="place-content-right">
-        <div className="place-directions">
-          <FaLocationDot />
-          <h4 className="country">{props.country}</h4>
-          <a href={props.map}><small>View on Google Maps</small></a>
+      <div className="place-content">
+        <img src={`./${props.img}`} alt="location" />
+        <div className="place-content-right">
+          <div className="place-directions">
+            <FaLocationDot color="rgb(255, 106, 0)" />
+            <h4 className="country">{props.country}</h4>
+            <a href={props.map}>
+              <small>View on Google Maps</small>
+            </a>
+          </div>
+          <h2>{props.place}</h2>
+          <h4 className="period">{props.period}</h4>
+          <p>
+            <small>{props.description}</small>
+          </p>
         </div>
-        <h2>{props.place}</h2>
-        <h4 className="period">{props.period}</h4>
-        <p><small>{props.description}</small></p>
       </div>
-    </div>
   );
 }
--- travel_journal/src/app/components/data.js
+++ travel_journal/src/app/components/data.js
@@ -7,5 +7,23 @@ export default[
         place: 'Mount Fuji',
         period: '12 Jan, 2021 - 24 Jan, 2021',
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-    }
+    },
+    {
+        id: 2,
+        img: 'sydney.jpeg',
+        country: 'Australia',
+        map: "https://www.google.com/maps/place/Mount+Fuji/@35.3606247,138.7186086,15z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634?shorturl=1",
+        place: 'Sydney Opera House',
+        period: '27 May, 2021 - 8 Jun, 2021',
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
+    },
+    {
+        id: 3,
+        img: 'geirangerfjord.jpeg',
+        country: 'Norway',
+        map: "https://www.google.com/maps/place/Mount+Fuji/@35.3606247,138.7186086,15z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634?shorturl=1",
+        place: 'Geirangerfjord',
+        period: '01 Oct, 2021 - 18 Nov, 2021',
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
+    },
 ]
